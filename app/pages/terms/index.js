36e65db589f89e0b033912f880616a6f719ff42b
John Azuka <gooption@yahoo.com>
2023-01-19 16:08:12 +0100

    19 Jan 2923, 4.07pm

--- app/pages/terms/index.js
+++ app/pages/terms/index.js
@@ -11,8 +11,29 @@ export const getStaticProps = async ({ locale }) => ({ props: { ...(await server
 import Breadcrumb from "../../layout/Breadcrumb/Breadcrumb";
 import FooterThree from "../../layout/footers/FooterThree";
 import BodyContent from "../../components/pages/terms_conditions/index";
+import axios from "axios";
 
 const TermsNConditions = () => {
+  const [homepageData, setHomePageData] = React.useState({ privacy: [],googleAdsScript:"" });
+
+
+  const fetchHomepageModels = async () => {
+    return await axios
+      .get(`/api/v1/fetchHomepageModels`)
+      .then((response) => response)
+      .catch((err) => err);
+  };
+
+  React.useEffect(
+    () =>
+      fetchHomepageModels()
+        .then((res) => {
+          setHomePageData({ ...res.data.userData });
+        })
+        .catch((err) => console.log(err)),
+
+    []
+  );
   return (
     <>
       <NavbarThree />
